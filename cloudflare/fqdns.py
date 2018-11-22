d24bdf0c7d9307c18c12a6dcf43250f0dee1ae66
Jakub Sokołowski <jakub@status.im>
2018-11-23 00:59:12 +0100

    remove unused code

--- cloudflare/fqdns.py
+++ cloudflare/fqdns.py
@@ -44,16 +44,7 @@ def main():
 
     zones = cf.zones.get(params={'per_page':100})
     zone = next(z for z in zones if z['name'] == opts.cf_domain)
-
-    zone_id = zone['id']
-
-    #settings_ssl = cf.zones.settings.ssl.get(zone_id)
-    #ssl_status = settings_ssl['value']
-    #settings_ipv6 = cf.zones.settings.ipv6.get(zone_id)
-    #ipv6_status = settings_ipv6['value']
-    #print(zone_id, zone_name, ssl_status, ipv6_status)
-
-    records = cf.zones.dns_records.get(zone_id, params={'per_page': 1000})
+    records = cf.zones.dns_records.get(zone['id'], params={'per_page': 1000})
 
     formatter = format_table
     if opts.csv:
