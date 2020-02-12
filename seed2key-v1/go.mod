e0194ef67c822a19eeb6ace75e24ff500eeb574c
Jakub Sokołowski <jakub@status.im>
2020-02-12 20:15:40 +0100

    add seed2key-v1
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ seed2key-v1/go.mod
@@ -0,0 +1,5 @@
+module seed2key-v1
+
+go 1.13
+
+require github.com/status-im/status-go v0.44.1 // indirect
---
+++ seed2key-v1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/status-im/status-go/extkeys"
+)
+
+func main() {
+	// read the seed phrase from stdin
+	reader := bufio.NewReader(os.Stdin)
+	readPhrase, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Printf("failed to read seed phrase from stdin\n")
+		os.Exit(1)
+	}
+	// trim whitespace
+	seedPhrase := strings.TrimSpace(readPhrase)
+	// convert the seed phrase to a private key
+	mnemonic := extkeys.NewMnemonic()
+	masterKey, err := extkeys.NewMaster(mnemonic.MnemonicSeed(seedPhrase, ""))
+	if err != nil {
+		fmt.Printf("cannot create master extended key: %v\n", err)
+		os.Exit(1)
+	}
+	// extract chat key from the master key
+	chatKey, err := masterKey.ChildForPurpose(extkeys.KeyPurposeWallet, 0)
+	if err != nil {
+		fmt.Printf("cannot extract chat key from master key: %v\n", err)
+		os.Exit(1)
+	}
+	// print the private key
+	fmt.Printf("%#x\n", crypto.FromECDSA(chatKey.ToECDSA()))
+}
