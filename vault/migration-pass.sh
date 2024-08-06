388bf9f474fbe9e902730db47f5388fd2340707d
Alexis Pentori <alexis@status.im>
2024-08-06 15:49:10 +0200

    vault: finishing migration script to vault
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- vault/migration-pass.sh
+++ vault/migration-pass.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Script to migrate secrets from password-store to vault instance
+#
 
 if [[ -z $VAULT_TOKEN ]]; then
   echo "Please set the varaible $VAULT_TOKEN"
@@ -12,9 +13,9 @@ if [[ -z $VAULT_ADDR ]]; then
 fi
 
 if [[ -z "${*}" ]]; then
-    echo "Usage: migration-pass.sh <bitwarden_path> <vault_path>"
+    echo "Usage: migration-pass.sh <pass_path> <vault_path>"
     echo
-    echo " - password_store_path: path of the secret in bitwarden"
+    echo " - pass_path: path of the secret in the password-store"
     echo " - vault_path: path of the secret in vault"
     echo 
     echo "Ex: migration-pass.sh fleet/waku/test/nodekeys waku/test/nodekeys"
@@ -24,17 +25,45 @@ if [[ -z "${*}" ]]; then
     exit 1
 fi
 
-BITWARDEN_PATH="$1"
+PASS_PATH="$1"
 VAULT_PATH="$2"
+VAULT="secret"
 
-if [[ -n $PASSWORD_STORE_DIR ]]; then
+if [[ -z $PASSWORD_STORE_DIR ]]; then
   echo "env var PASSWORD_STORE_DIR not set, using default location ~/.password-store"
-  PASSWORD_STORE_DIR=$HOME/.password-store/
+  PASSWORD_STORE_DIR=$HOME/.password-store
 fi
 
 declare -a params
 
-if [[ ! -f "$PASSWORD_STORE_DIR/$VAULT_PATH" ]]; then
-  # Extract last part of the path, push into a 
-  params="$name=$(pass VAULT_PATH)"
+
+if [[ -f "$PASSWORD_STORE_DIR/$PASS_PATH.gpg" ]]; then
+  echo "Reading file $PASS_PATH"
+  field_name=$(basename $PASS_PATH)
+  echo $field_name
+  jq_param=".\"${field_name/./_}\"=\"$(pass $PASS_PATH)\""
+  echo $jq_param
+  params=$(echo "{}" | jq "${jq_param}")
 fi
+
+
+if [[ -d "$PASSWORD_STORE_DIR/$PASS_PATH" ]]; then
+  echo "Reading all file in directory $PASS_PATH"
+  params="{}"
+  files=$(ls "$PASSWORD_STORE_DIR/$PASS_PATH/")
+  while IFS= read -r file; do
+    echo "File name ${file}"
+    if [[ -f "$PASSWORD_STORE_DIR/$PASS_PATH/$file" ]]; then
+      field_name=${file/.gpg/}
+      jq_query=".\"${field_name/./_}\"=\"$(pass $PASS_PATH/${field_name})\""
+      echo 
+      #echo ${jq_query/\n/\\n}
+      echo 
+      params=$(echo $params | jq "${jq_query}")
+    fi
+    echo
+    echo "params: $params"
+  done <<< "$files"
+fi
+
+echo "$params" | vault kv put -mount=${VAULT} ${VAULT_PATH} -
