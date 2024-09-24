454ad109c8782e48362cdd9762b72f7ea91b0b5e
Jakub Sokołowski <jakub@status.im>
2024-09-24 11:53:04 +0200

    git/sync_template.sh: add template changes to fleet repos
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ git/sync_template.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function help() {
+    echo 'Usage: ./sync_template.sh "my commit message" file/to/copy another/file/to/copy ...'
+}
+
+[[ "$#" -lt 2 ]] && { help; exit 1; }
+
+# First argument is the message
+COMMIT_MESSAGE="${1}"
+shift
+
+for DIR in $(ls -d ~/work/infra-*); do
+    [[ "${DIR}" =~ .*/infra-role-.*$ ]] && continue
+    [[ "${DIR}" =~ .*/infra-tf-.*$ ]] && continue
+    [[ "${DIR}" =~ .*/infra-(utils|pass)$ ]] && continue
+    echo '---------------------------------'
+    pushd "${DIR}"
+    git reset
+    git stash -u
+    git fetch --verbose origin $(git rev-parse --abbrev-ref HEAD)
+    git rebase origin/$(git rev-parse --abbrev-ref HEAD)
+    for FILE in $@; do
+        cp "${HOME}/work/infra-template/${FILE}" "./${FILE}"
+        git add "./${FILE}"
+    done
+    git commit -m "${COMMIT_MESSAGE}"
+    git push
+    git stash apply
+    popd
+done
