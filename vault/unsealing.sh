1040be693defb96a34963d7f3e4c3dab09455db4
Alexis Pentori <alexis@status.im>
2024-10-16 11:51:50 +0200

    vaut: update output of unsealing script
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- vault/unsealing.sh
+++ vault/unsealing.sh
@@ -28,19 +28,26 @@ fi
 
 vault_addr=""
 echo "Fetching Vault address from Consul"
+echo ""
 readarray -t DCS < <(curl -s "${CONSUL_URL}/datacenters" | jq -r '.[]')
 for DC in "${DCS[@]}"; do
     vault_addr=$(echo $vault_addr $(curl -s "${CONSUL_URL}/service/vault?dc=${DC}" | jq -r '.[].Node'))
 done
-
-echo $vault_addr
-
+echo "Calling each Vault"
+echo ""
+result='[]'
 for addr in $vault_addr; do
   export VAULT_ADDR="https://$addr.status.im:8200"
-  status=$(vault status -format=json| jq -r ".sealed")
-  echo "Vault on $addr is $([ "$status" == 'false' ] && echo 'unsealed' || echo 'sealed')"
-  if [[ $status == "true" && $unseal == 1 ]]; then
+  status=$(vault status -format=json)
+  elt=$(echo $status | jq -r "{hostname:\"$addr\",sealed:.sealed,progress:.progress}")
+  result=$(echo $result| jq ".+=[$elt]")
+  sealed=$(echo $status | jq -r ".sealed")
+  if [[ $sealed == "true" && $unseal == 1 ]]; then
     echo "Unsealing the host $addr"
     vault operator unseal
   fi
 done
+
+echo "Vaults status"
+echo ""
+echo "$result" | jq -r '.[] | [.hostname, .sealed, .progress] | @csv' | column -s, -t -N Hostname,Sealed,Progress
