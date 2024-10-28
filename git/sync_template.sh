d26843136934ace342e011cbf8042eee06576945
Jakub Sokołowski <jakub@status.im>
2024-10-28 12:50:35 +0100

    git/sync_template.sh: improve script to be safer
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- git/sync_template.sh
+++ git/sync_template.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+set -x
 
 function help() {
     echo 'Usage: ./sync_template.sh "my commit message" file/to/copy another/file/to/copy ...'
@@ -13,19 +15,21 @@ shift
 for DIR in $(ls -d ~/work/infra-*); do
     [[ "${DIR}" =~ .*/infra-role-.*$ ]] && continue
     [[ "${DIR}" =~ .*/infra-tf-.*$ ]] && continue
-    [[ "${DIR}" =~ .*/infra-(lido|utils|pass)$ ]] && continue
-    echo '---------------------------------'
-    pushd "${DIR}"
+    [[ "${DIR}" =~ .*/infra-(lido|utils|pass|docs|template)$ ]] && continue
+    [[ -f "${DIR}/.envrc.secrets" ]] || continue
+    echo "------------------------------------------------>>> ${DIR}"
+    pushd "${DIR}" >/dev/null
+    git checkout master
     git reset
-    git stash -u
+    git status -s >/dev/null && git stash -u
     git fetch --verbose origin $(git rev-parse --abbrev-ref HEAD)
     git rebase origin/$(git rev-parse --abbrev-ref HEAD)
     for FILE in $@; do
-        cp "${HOME}/work/infra-template/${FILE}" "./${FILE}"
+        cp "${HOME}/work/infra-template/${FILE}" "${PWD}/${FILE}"
         git add "./${FILE}"
     done
-    git commit -m "${COMMIT_MESSAGE}"
-    git push
-    git stash apply
-    popd
+    git status
+    git commit -m "${COMMIT_MESSAGE}" && { git show --stat; git push; }
+    git stash apply || true
+    popd >/dev/null
 done
