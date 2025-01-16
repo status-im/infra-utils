713363a40275c00bab4832017750c689c8b23d8f
Alexis Pentori <alexis@status.im>
2025-01-16 09:40:03 +0100

    twitter: improve script to allow user retry
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- twitter/oauth.py
+++ twitter/oauth.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 import tweepy
 import os
+import logging
+import requests
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 """
-Script to generate token to allow twitter dev account to 
+Script to generate token to allow twitter dev account to
 use API as another twitter account.
 
 Require tweepy dependency
-Use for airbyte connector 
+Use for airbyte connector
 """
 
 def print_help():
-    print("The variables CLIENT_ID, CLIENT_SECRET and REDIRECT_URI must be set")
-    print("The CLIENT_ID and CLIENT_SECRET are generated when creating a twitter App : https://developer.twitter.com/en/portal/projects-and-apps")
-    print("The REDIRECT_URI is set throught the User Authentication settings in the twitter app and must be in https")
+    logger.info("The variables CLIENT_ID, CLIENT_SECRET and REDIRECT_URI must be set")
+    logger.info("The CLIENT_ID and CLIENT_SECRET are generated when creating a twitter App : https://developer.twitter.com/en/portal/projects-and-apps")
+    logger.info("The REDIRECT_URI is set throught the User Authentication settings in the twitter app and must be in https")
 
 if "CLIENT_ID" not in os.environ:
     print_help()
@@ -26,21 +31,33 @@ if "REDIRECT_URI" not in os.environ:
     exit(1)
 
 oauth2_user_handler = tweepy.OAuth2UserHandler(
-    client_id= os.environ.get("CLIENT_ID"),
+    client_id=os.environ.get("CLIENT_ID"),
     redirect_uri=os.environ.get("REDIRECT_URI"),
-    scope=["tweet.read", "users.read", "tweet.write", "offline.access"],
+    scope=[
+        "tweet.read",
+        "users.read",
+        "offline.access"
+    ],
     client_secret=os.environ.get("CLIENT_SECRET")
 )
 
-print("Copy this url in a browser logged in the Twitter account.")
 print(oauth2_user_handler.get_authorization_url())
+authed = False
+while not authed:
+    try:
+        authorization_response_url=input("Enter the authorization response URL. ")
+        logger.info("URL : %s", authorization_response_url)
+        access_token = oauth2_user_handler.fetch_token(
+            authorization_response_url
+        )
+        authed = True
+    except Exception as e:
+        logger.error("Something weird happend, please restart %s", e)
 
-authorization_response_url=input("Enter the authorization response URL. ")
+logger.info("Access token : %s", access_token)
+headers={"Authorization": f"Bearer {access_token.get('access_token')}"}
+logger.info("Header %s", headers)
 
-print("URL : %s" % authorization_response_url)
+resp = requests.get("https://api.x.com/2/users/me", headers=headers)
 
-access_token = oauth2_user_handler.fetch_token(
-    authorization_response_url
-)
-
-print("Access token : %s" % access_token)
+logger.info("User infos : %s", resp.json().get("data"))
