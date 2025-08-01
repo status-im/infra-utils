71d83492f5358ef7c8ec9a70f83b03cbe10e2a7e
Jakub Sokołowski <jakub@status.im>
2025-08-01 17:08:17 +0200

    consul/get_services.sh: parse metadata names from args
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- consul/get_services.sh
+++ consul/get_services.sh
@@ -2,13 +2,25 @@
 
 CONSUL_URL="${CONSUL_URL:-http://localhost:8500/v1/catalog}"
 SERVICE="${1}"
+shift
 
-[[ -z "${SERVICE}" ]] && { echo "No service name given!" >&2; exit 1; }
+if [[ -z "${SERVICE}" ]]; then
+    echo "USAGE: ${0} <service_name> [service_metadata_names|...]" >&2
+    exit 1
+fi
+
+# Collect service metadata names
+EXTRA_COLUMNS=('.ServiceMeta.version')
+for NAME in ${@}; do
+    EXTRA_COLUMNS+=(".ServiceMeta.\"${NAME}\"")
+done
+
+function join() { for i in "${@}"; do echo -n ", $i"; done; echo; }
 
 readarray -t DCS < <(curl -s "${CONSUL_URL}/datacenters" | jq -r '.[]')
 
 for DC in "${DCS[@]}"; do
     curl -s "${CONSUL_URL}/service/${SERVICE}?dc=${DC}"
 done \
-    | jq -r '.[] | [.Node, .ServiceName, .ServiceID, .ServiceMeta.version] | @tsv' \
+    | jq -r ".[] | [.Node, .ServiceName, .ServiceID$(join "${EXTRA_COLUMNS[@]}")] | @tsv" \
     | column -t
