940ed193ca0142304026b1b2beff8e0556fdbb22
Jakub Sokołowski <jakub@status.im>
2026-03-18 10:39:10 +0100

    git/sync_template.sh: add support for message from file
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- git/sync_template.sh
+++ git/sync_template.sh
@@ -17,6 +17,15 @@ STASHED=0
 COMMIT_MESSAGE="${1}"
 shift
 
+# Support loading big message from a file
+if [[ -r "${COMMIT_MESSAGE}" ]]; then
+    COMMIT_TITLE=$(cat "${COMMIT_MESSAGE}" | head -n1)
+    COMMIT_MESSAGE_ARG="-F ${COMMIT_MESSAGE}"
+else
+    COMMIT_TITLE=$(echo "${COMMIT_MESSAGE}" | head -n1)
+    COMMIT_MESSAGE_ARG="-m ${COMMIT_MESSAGE}"
+fi
+
 for DIR in $(ls -d ~/work/infra-*); do
     [[ "${DIR}" =~ .*/infra-role-.*$ ]] && continue
     [[ "${DIR}" =~ .*/infra-tf-.*$ ]] && continue
@@ -24,7 +33,7 @@ for DIR in $(ls -d ~/work/infra-*); do
     [[ -f "${DIR}/.envrc.secrets" ]] || continue
 
     echo "------------------------------------------------>>> ${DIR}"
-    if git -C "${DIR}" log --grep "${COMMIT_MESSAGE}" | grep commit >/dev/null; then
+    if git -C "${DIR}" log --grep "${COMMIT_TITLE}" | grep commit >/dev/null; then
         echo "Commit already exists."
         git -C "${DIR}" status -sb
         [[ "${GIT_PUSH}" -eq 1 ]] && git -C "${DIR}" push
@@ -49,7 +58,7 @@ for DIR in $(ls -d ~/work/infra-*); do
     if git diff --cached --quiet; then
         echo "Nothing staged."
     else
-        git commit -m "${COMMIT_MESSAGE}" && { git show --stat; }
+        git commit ${COMMIT_MESSAGE_ARG} && { git show --stat; }
     fi
 
     if [[ "${STASHED}" -eq 1 ]]; then
